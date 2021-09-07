10f0e05fe188a355a6b4dc41824be0882ad8f598
aniketparate <aniketvparate@gmail.com>
2021-09-07 11:25:44 +0530

    first commit

---
+++ src/basicIO/firstprint.java
@@ -0,0 +1,14 @@
+package basicIO;
+
+public class firstprint {
+    public static void main(String[] args) {
+        //Escape sequence
+        // \n - new line
+        // \t - tab
+        System.out.println("WELCOME");
+        System.out.print("This is a line\n");
+        System.out.print("This is another line.\n");
+        System.out.println("I know what you are thing...\t\tpineapple");
+
+    }
+}
---
+++ src/basicIO/variables.java
@@ -0,0 +1,19 @@
+package basicIO;
+
+public class variables {
+    public static void main(String[] args) {
+        //How to use different values in java.
+        //String, integers, double and float.
+        //Data types.
+        //Use it in a variable.
+
+        int age = 10;
+        int num1 = 5, num2 = 10;
+        int sum = num1 + num2;
+
+        System.out.println("This is int datatype :");
+        System.out.println("The age is " + age);
+        System.out.println("The sum is " + sum);
+        System.out.println("The sum is " + (num1 + num2));
+    }
+}
