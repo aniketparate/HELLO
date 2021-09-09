1fb3b5f9c6c9b9dc6c26939c57f1745510efbab7
aniketparate <aniketvparate@gmail.com>
2021-09-09 14:22:13 +0530

    first commit

--- src/basic/arrays.java
+++ src/basic/arrays.java
@@ -1,4 +1,17 @@
 package basic;
 
 public class arrays {
+
+    public static void main(String[] args) {
+        int age = 30;
+        int physics = 97;
+        int chem = 98;
+        int eng = 95;
+
+        int[] marks = new int[3];
+        marks[0] = 97;
+        marks[1] = 98;
+        marks[2] = 95;
+
+    }
 }
