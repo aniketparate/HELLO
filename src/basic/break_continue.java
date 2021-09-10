a2da962ce191a613d45ffc3291eb8f0f07ffe7f1
aniketparate <aniketvparate@gmail.com>
2021-09-10 23:15:07 +0530

    add

---
+++ src/basic/break_continue.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class break_continue {
+
+    public static void main(String[] args) {
+        //BREAK & CONTINUE
+        int i = 0;
+        while(true){
+            if (i == 3) {
+                i = i + 1;
+                continue;
+            }
+            System.out.println(i);
+            i = i +1;
+            if (i > 5) {
+                break;
+            }
+        }
+    }
+}
