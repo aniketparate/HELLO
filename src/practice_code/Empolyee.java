2c4231d46e3d240ec36f55e54a28b4178ded48a3
aniketparate <aniketvparate@gmail.com>
2021-10-07 14:23:31 +0530

    update code

--- src/practice_code/Empolyee.java
+++ src/practice_code/Empolyee.java
@@ -5,50 +5,59 @@ import java.util.Scanner;
 class employee {
     Scanner sc = new Scanner(System.in);
 
-    double Employee_no;
-    String Empolyee_name;
-    double basic;
-    double DA;
-    double ID;
-    double IT;
-    double netSalary;
+    int size;
+    double[] Employee_no = new double[size];
+    String[] Employee_name = new String[size];
+    double[] basic = new double[size];
+    double[] DA = new double[size];
+    double[] ID = new double[size];
+    double[] IT = new double[size];
+    double[] netSalary = new double[size];
 
     void employeeDetails () {
-        System.out.print(" Enter Employee No : ");
-        Employee_no = sc.nextInt();
-        System.out.print(" Enter Employee Name : ");
-        Empolyee_name = sc.next();
-        System.out.print(" Enter Employee ID : ");
-        ID= sc.nextInt();
-        System.out.print(" Enter basic : ");
-        basic = sc.nextInt();
-        System.out.println();
+        System.out.print(" Enter no of entries : ");
+        size = sc.nextInt();
+        ///int size = this.size;
+        for (int i = 0; i < size; i++) {
+            System.out.print(" Enter Employee No : ");
+            Employee_no[i] = sc.nextInt();
+            System.out.print(" Enter Employee Name : ");
+            Employee_name[i] = sc.next();
+            System.out.print(" Enter Employee ID : ");
+            ID[i]= sc.nextInt();
+            System.out.print(" Enter basic : ");
+            basic[i] = sc.nextInt();
+            System.out.println();
+        }
     }
 
     void netSalary(){
-        DA = 1.32 * basic;
-        IT = 0.3 * (basic + DA);
-        netSalary = (basic + DA) - IT;
+        for (int i = 0; i < size; i++) {
+            DA[i] = 1.32 * basic[i];
+            IT[i] = 0.3 * (basic[i] + DA[i]);
+            netSalary[i] = (basic[i] + DA[i]) - IT[i];
+        }
     }
 
     void display() {
-        System.out.println(" Employee No : " + Employee_no);
-        System.out.println(" Employee Name : " + Empolyee_name);
-        System.out.println(" ID : " + ID);
-        System.out.println(" Basic : " + basic);
-        System.out.println(" DA : " + DA);
-        System.out.println(" IT : " + IT);
-        System.out.println(" Net Salary : " + netSalary);
+        for (int i = 0; i < size; i++) {
+            System.out.println(" Employee No : " + Employee_no[i]);
+            System.out.println(" Employee Name : " + Employee_name[i]);
+            System.out.println(" ID : " + ID[i]);
+            System.out.println(" Basic : " + basic[i]);
+            System.out.println(" DA : " + DA[i]);
+            System.out.println(" IT : " + IT[i]);
+            System.out.println(" Net Salary : " + netSalary[i]);
+        }
     }
 }
 
 public class Empolyee {
     public static void main(String[] args) {
-        employee employ = new employee();
+        employee emp = new employee();
 
-        employ.employeeDetails();
-        employ.netSalary();
-        employ.display();
-
+        emp.employeeDetails();
+        emp.netSalary();
+        emp.display();
     }
 }
